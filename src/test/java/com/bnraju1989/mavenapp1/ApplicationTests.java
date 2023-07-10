ed8bda178a095e989452c6fb06ccd8c98d7a1ede
mallikaraju <marri.mallika@gmail.com>
2023-07-10 13:33:50 +0530

    first commit

---
+++ src/test/java/com/bnraju1989/mavenapp1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnraju1989.mavenapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
